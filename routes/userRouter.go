b2f44cf1faac28f0ef15e902bedee4c9f55b318f
belokmaster <sychev.nickolay1@gmail.com>
2024-11-05 20:54:47 +0300

    add controllers

--- routes/userRouter.go
+++ routes/userRouter.go
@@ -7,7 +7,7 @@ import (
 
 func UserRoutes(incomingRoutes *gin.Engine) {
 	incomingRoutes.GET("/users", controller.GetUsers())
-	incomingRoutes.GET("/users/:user_id", controller.GetUsers())
-	incomingRoutes.POST("/users/signup", controller.Signup())
+	incomingRoutes.GET("/users/:user_id", controller.GetUser())
+	incomingRoutes.POST("/users/signup", controller.SignUp())
 	incomingRoutes.POST("/users/login", controller.Login())
 }
