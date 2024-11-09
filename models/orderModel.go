6e6b9a5e9c546786795f2a8eda1c5418e4e68ecd
belokmaster <sychev.nickolay1@gmail.com>
2024-11-09 18:37:17 +0300

    fix some things

--- models/orderModel.go
+++ models/orderModel.go
@@ -2,14 +2,15 @@ package models
 
 import (
 	"time"
+
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-type MENU struct {
-	ID primitive.ObjectID `bson:"_id"`
-	Oder_date time.Time `json:"order_date" validate:"required"`
-	Created_at time.Time `json:"created_at"`
-	Updated_at time.Time `json:"updated_at"`
-	Order_id string `json:"order_id"`
-	Table_id *string `json:"table_id" validate:"required"` 
+type Order struct {
+	ID         primitive.ObjectID `bson:"_id"`
+	Oder_date  time.Time          `json:"order_date" validate:"required"`
+	Created_at time.Time          `json:"created_at"`
+	Updated_at time.Time          `json:"updated_at"`
+	Order_id   string             `json:"order_id"`
+	Table_id   *string            `json:"table_id" validate:"required"`
 }
